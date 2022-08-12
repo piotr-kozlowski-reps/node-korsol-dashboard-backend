567529d4e74319092a193d7bb89654d33671f27a
Piotr Kozlowski <pegaz@ante.pl>
2022-08-12 22:25:26 +0200

    companies ...

--- models/companiesDummy.js
+++ models/companiesDummy.js
@@ -1,35 +1,403 @@
 const companiesData = [
   {
-    id: "8eofvevervoe",
-    name: "Firma 1",
+    id: "f1efe069-d5a3-497b-89f9-3b63cfd414fe",
+    name: "Eimbee",
   },
   {
-    id: "8eofvevervoeSDCAS",
-    name: "Firma 2",
+    id: "612a1237-d8fe-4784-af8e-99311a65a7ed",
+    name: "Tavu",
   },
   {
-    id: "8WDCDCeofvevervoe",
-    name: "Firma 3",
+    id: "3836f0af-98ca-42a6-aabb-96cffd64bd71",
+    name: "Trudeo",
   },
   {
-    id: "8eofveASDC343vervoe",
-    name: "Firma 4",
+    id: "db2fef34-9e86-4e7c-b331-acc20326ca80",
+    name: "Fivechat",
   },
   {
-    id: "8eERTofveASDC343vervoe",
-    name: "Firma 5",
+    id: "2c83f2e5-faa3-4313-92e4-8ad2fcba4c5e",
+    name: "Twitternation",
   },
   {
-    id: "8eofveADC343vervoe",
-    name: "Firma 6",
+    id: "1a60fe1a-aeef-49f4-8cc1-1f466abdcc07",
+    name: "Skinte",
   },
   {
-    id: "8eofvEDFeASDCASD343vervoe",
-    name: "Firma 7",
+    id: "6d378a8c-49ca-42d6-ad55-c392c2145cf2",
+    name: "Tekfly",
   },
   {
-    id: "8eofveASDC343veADrvoe",
-    name: "Firma 8",
+    id: "92047e5b-cfb2-478c-a37f-1a003357e33b",
+    name: "Twitterworks",
+  },
+  {
+    id: "3cee6ca7-bee9-4dba-9720-4f5b7893595b",
+    name: "Babbleset",
+  },
+  {
+    id: "dcb9fefb-cb79-4d10-b2ac-f41df1691328",
+    name: "Browsezoom",
+  },
+  {
+    id: "5d8fa702-5b54-4675-b020-e0b3e627f771",
+    name: "Linkbridge",
+  },
+  {
+    id: "5951d912-c718-417f-8b9e-e005cfb9f3a7",
+    name: "Eabox",
+  },
+  {
+    id: "d49ab34a-14e4-449c-9858-3b059b589a40",
+    name: "Snaptags",
+  },
+  {
+    id: "85ea0507-1dfa-486b-a895-e140435d68cd",
+    name: "Eare",
+  },
+  {
+    id: "aca2cb74-9e1e-4785-9d02-68bfbf2d5325",
+    name: "Yombu",
+  },
+  {
+    id: "eeaa282e-9018-4d7f-8d6f-c9f9f4d4cb5c",
+    name: "Gabspot",
+  },
+  {
+    id: "c7c8b615-15d0-4515-8f75-985063efb698",
+    name: "Agimba",
+  },
+  {
+    id: "82691756-c843-4f58-bc57-16a34f9c770e",
+    name: "Mynte",
+  },
+  {
+    id: "7c55133e-14bd-48ec-9e99-d2f663d4d814",
+    name: "Podcat",
+  },
+  {
+    id: "aa038e2e-76f0-4393-b4b2-75cd1a021bf1",
+    name: "Quinu",
+  },
+  {
+    id: "b271966c-8da1-45ab-94eb-bdac91930af3",
+    name: "Yata",
+  },
+  {
+    id: "9ed042eb-7a44-4e36-9f67-78df048bc2a3",
+    name: "Kaymbo",
+  },
+  {
+    id: "5fed655f-aa7b-495d-8b17-10c5c9ecccfc",
+    name: "Browsezoom",
+  },
+  {
+    id: "f8dcbb56-826d-47ae-ae7d-e8a0dbfb604b",
+    name: "Minyx",
+  },
+  {
+    id: "8a768c70-d630-498a-ae8c-eaf5f108c25c",
+    name: "Brightdog",
+  },
+  {
+    id: "9a02b959-25d1-4e3b-a8dc-a1e996cd81b4",
+    name: "Wordtune",
+  },
+  {
+    id: "a6afa340-1339-4f45-99c2-e022e70ca001",
+    name: "Cogidoo",
+  },
+  {
+    id: "a89dddff-be2d-4cff-98fc-ea84b13aa234",
+    name: "Edgeblab",
+  },
+  {
+    id: "eab5938c-7984-4cc1-9d6c-af2439d13108",
+    name: "Vipe",
+  },
+  {
+    id: "cad4a678-b0f2-4c87-a392-44cbed4ce690",
+    name: "Gigashots",
+  },
+  {
+    id: "134f7f20-509c-4bb2-bbef-46530cd32872",
+    name: "Jabbercube",
+  },
+  {
+    id: "d1315dcb-4988-4561-ad5c-ca3f86753cc0",
+    name: "Skynoodle",
+  },
+  {
+    id: "b252479d-7bad-4981-98e4-2f7323b2d892",
+    name: "Dabvine",
+  },
+  {
+    id: "3e3939e1-392a-42b1-8b87-29797d109941",
+    name: "Bluezoom",
+  },
+  {
+    id: "1f302893-c0b5-4e78-b722-7a36dc2c5124",
+    name: "Mydeo",
+  },
+  {
+    id: "1d838f3d-0eed-4925-afde-beb6e47bfcd0",
+    name: "Ailane",
+  },
+  {
+    id: "07e12f71-d2c0-46c8-8368-fd3940833daa",
+    name: "Midel",
+  },
+  {
+    id: "7959b3d7-1b7a-4c0d-86c7-f708975e0aa1",
+    name: "Kayveo",
+  },
+  {
+    id: "4712eac0-c3ab-4390-9e67-12ea8ebffd18",
+    name: "Minyx",
+  },
+  {
+    id: "62ea8035-6512-4a67-a046-424f1cada15b",
+    name: "Jabbertype",
+  },
+  {
+    id: "88a574ae-2597-4d2b-b99a-c8d052841ef2",
+    name: "Shufflester",
+  },
+  {
+    id: "db1b5b71-8d1d-4a7f-8678-a3cf31ed1b53",
+    name: "Twimbo",
+  },
+  {
+    id: "85cb9e1e-872d-4594-bc00-7808d0c9910d",
+    name: "Blogpad",
+  },
+  {
+    id: "865ac26c-5d1f-42dd-a79f-47c811ae7624",
+    name: "Brainverse",
+  },
+  {
+    id: "8ab5bfcc-022b-42f3-a30d-c71289b0b3f7",
+    name: "Eimbee",
+  },
+  {
+    id: "22c7ec50-0de4-4780-91f1-a1d68fcb57cb",
+    name: "Thoughtbeat",
+  },
+  {
+    id: "8e6775fc-2548-4cc0-91a1-2aa47bcabe38",
+    name: "Gabspot",
+  },
+  {
+    id: "78e7b6a2-1148-4a56-b38b-d063008adc67",
+    name: "Browsebug",
+  },
+  {
+    id: "feaa0c5f-42c9-470d-902a-ea32f898f190",
+    name: "Kwimbee",
+  },
+  {
+    id: "eca12ac4-22ab-4e7b-83fa-9723595e7169",
+    name: "Bubblebox",
+  },
+  {
+    id: "14a082ec-f360-4578-bb6f-068ad8497fd2",
+    name: "Meevee",
+  },
+  {
+    id: "2fc03515-b153-4164-857f-16745744386c",
+    name: "Jaxnation",
+  },
+  {
+    id: "f16649b6-f3c3-4cf6-828d-8d1919a14e60",
+    name: "Bubblemix",
+  },
+  {
+    id: "3ef315d7-161f-4f33-8c2b-196eea951e89",
+    name: "Jamia",
+  },
+  {
+    id: "3a965af4-a538-473b-ba46-d22725e8bce1",
+    name: "Wordpedia",
+  },
+  {
+    id: "8764dafe-405d-4679-b53f-cb5af1e4c19c",
+    name: "Pixonyx",
+  },
+  {
+    id: "53754459-55da-492e-aa70-cf97ee5c8593",
+    name: "Wordify",
+  },
+  {
+    id: "f2a34ca1-450f-491c-a553-b963aa2bc0d0",
+    name: "Camido",
+  },
+  {
+    id: "bc45323b-9631-400a-a65a-64df1c29c76c",
+    name: "Riffwire",
+  },
+  {
+    id: "73665fb9-dfc8-47e1-a4d8-e1af0336976d",
+    name: "Browsezoom",
+  },
+  {
+    id: "cfab147e-2911-4e42-a517-87e7c4aa1cc1",
+    name: "Livetube",
+  },
+  {
+    id: "c664e530-d3a1-4a4c-8fd3-fedcc13045cc",
+    name: "Livetube",
+  },
+  {
+    id: "aba646aa-33af-4d95-af74-916a64b2e8e0",
+    name: "Feedfire",
+  },
+  {
+    id: "b2f5d3e9-5b19-4966-b9f4-99f4de156e9b",
+    name: "Pixonyx",
+  },
+  {
+    id: "fc7bd03b-38ff-45d0-81b9-f9d802e24c24",
+    name: "Jabbercube",
+  },
+  {
+    id: "fdb9418b-881c-4e60-bb24-0e19d4041b1c",
+    name: "Quinu",
+  },
+  {
+    id: "0590cc8f-7109-4df6-b176-171e05652300",
+    name: "Photolist",
+  },
+  {
+    id: "b6240e1f-b298-4bda-b839-3dcd88de0868",
+    name: "Divavu",
+  },
+  {
+    id: "b408f7f7-2536-440e-a04d-c62b16642748",
+    name: "Twinte",
+  },
+  {
+    id: "71903674-c46c-4217-b766-556c15b19b61",
+    name: "Topdrive",
+  },
+  {
+    id: "2cc4c3f0-5efe-47da-ad1d-49c513715778",
+    name: "Fivebridge",
+  },
+  {
+    id: "4890a3c0-27df-4102-8bfb-4584c7d51d40",
+    name: "Photofeed",
+  },
+  {
+    id: "d4803e1e-30b1-4496-a7f6-0ca84e160b2b",
+    name: "Mudo",
+  },
+  {
+    id: "f87d68c5-81c6-4ffe-b043-31120eff55fe",
+    name: "Skilith",
+  },
+  {
+    id: "fe2e54c9-b789-475e-a40d-cf2dd123f4dc",
+    name: "Livetube",
+  },
+  {
+    id: "e74c89ee-1d57-43c3-8114-de1d0cb71c31",
+    name: "Jayo",
+  },
+  {
+    id: "36e341a4-9505-43e2-b642-282cc1847662",
+    name: "Innojam",
+  },
+  {
+    id: "eb951c5d-ef69-4140-a138-21f816c17997",
+    name: "Voonyx",
+  },
+  {
+    id: "bb5b94e7-af3f-4ae5-87fa-e84770a24856",
+    name: "Shufflester",
+  },
+  {
+    id: "8c916f60-b450-42f0-ba42-5dbeecbd68f8",
+    name: "Mycat",
+  },
+  {
+    id: "b6522214-0652-42e5-9c99-fe33b29052ce",
+    name: "Yodel",
+  },
+  {
+    id: "7cd660d5-47c0-48e9-a740-ee7dcf5334f9",
+    name: "Quatz",
+  },
+  {
+    id: "2ef31253-3b03-4343-aa64-8897f4c1247d",
+    name: "Babbleopia",
+  },
+  {
+    id: "a2c25656-e6b7-4399-a3e8-a8e0e010654e",
+    name: "Livetube",
+  },
+  {
+    id: "f8590d4d-48e3-49ff-8987-1de261c17a24",
+    name: "Dablist",
+  },
+  {
+    id: "b9a4f3b6-c43a-4df6-869c-e6dcd3dad41f",
+    name: "Thoughtworks",
+  },
+  {
+    id: "cc239651-63d2-4144-a256-06ca7dda68d4",
+    name: "Yambee",
+  },
+  {
+    id: "8430660e-29f4-4810-b62d-fc0ca0c5f9fa",
+    name: "Skimia",
+  },
+  {
+    id: "96040974-8b79-4a0a-bd8c-27cb2e861aee",
+    name: "Meeveo",
+  },
+  {
+    id: "ad7ef343-1096-486f-8499-f60a7466a903",
+    name: "Twimbo",
+  },
+  {
+    id: "679b5b28-a296-4652-be77-e57ac0277955",
+    name: "Jaloo",
+  },
+  {
+    id: "9a3dfcb2-ba98-4b93-b286-7e535948b8a1",
+    name: "Pixoboo",
+  },
+  {
+    id: "bd995044-7517-474b-9a3d-e80497bc8d9f",
+    name: "Wikizz",
+  },
+  {
+    id: "fde0d42b-a7a4-4ffc-9306-f51c56a62e6e",
+    name: "Yacero",
+  },
+  {
+    id: "eb333483-e231-4787-8d25-7eec9e4d570b",
+    name: "Aimbo",
+  },
+  {
+    id: "fa777bba-ef89-45e5-9041-fbce384e4dd1",
+    name: "Tekfly",
+  },
+  {
+    id: "9cb3b0e1-aeef-4602-aa40-c9d6db379924",
+    name: "Demimbu",
+  },
+  {
+    id: "51a0fc4d-c645-4eb4-8077-9ca9b6ed1d7d",
+    name: "InnoZ",
+  },
+  {
+    id: "54cde25c-de36-4fa0-83e0-c85f7e680798",
+    name: "Flipopia",
+  },
+  {
+    id: "d3aaf2de-40fa-49ac-91d5-0bb686190cda",
+    name: "Chatterbridge",
   },
 ];
 
